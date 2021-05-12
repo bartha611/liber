3e68dd2c934e1ed83002e89da78c769390e1b4b1
Adam Barth <adambarth611@gmail.com>
2021-05-12 02:33:31 +0000

    Changed settings.py for production

--- Liber/settings.py
+++ Liber/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG")
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','localhost', 'liber611.com', 'www.liber611.com']
 
 AUTH_USER_MODEL = "authentication.User"
 
